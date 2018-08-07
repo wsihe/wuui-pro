81c917db33d0b5ccbfd7aa4e91a578dc5dff588d
river <837606100@qq.com>
2018-08-07 16:33:24 +0800

    [项目] 修改config

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const fs = require('fs')
+
+const resolve = dir => {
+  return path.join(__dirname, dir)
+}
+
+const env = process.env.NODE_ENV || 'development'
+
+fs.writeFileSync(path.join(__dirname, './config/env.js'), `export default '${env}'`)
+
+const BASE_URL = '/'
+
+module.exports = {
+  baseUrl: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('components', resolve('src/components'))
+      .set('config', resolve('config'))
+  },
+  // 打包时不生成.map文件
+  productionSourceMap: false
+}
