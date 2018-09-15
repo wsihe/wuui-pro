708e1a54dbbc33a462f477749c412e686196d322
river <837606100@qq.com>
2018-09-15 22:17:19 +0800

    [项目] 添加design组件

---
+++ src/components/design/richtext.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  .wu-richtext
+    div(:id="")
+</template>
+
+<script>
+export default {
+  name: 'WuDesign',
+  components: {},
+  props: {
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  mounted () {
+  },
+  destroyed () {
+  },
+  computed: {
+  },
+  watch: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="stylus">
+  .wu-richtext
+    display blcok
+</style>
