8d3ce408abbafdce13809d3bed4d322a870965b5
river <837606100@qq.com>
2018-09-18 16:23:19 +0800

    [项目] 添加分页组件

---
+++ src/components/pagination/pagination.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  .wu-pagination
+    el-pagination(
+      @current-change='handleCurrentChange',
+      :current-page.sync='currentPage',
+      :page-size='pageSize',
+      layout='total, prev, pager, next, jumper',
+      :total='total')
+</template>
+
+<script>
+export default {
+  name: 'WuPagination',
+  components: {},
+  props: {
+  },
+  data () {
+    return {
+      currentPage: 1,
+      total: 0
+    }
+  },
+  created () {
+  },
+  mounted () {
+  },
+  destroyed () {
+  },
+  computed: {
+  },
+  watch: {
+  },
+  methods: {
+    handleCurrentChange (val) {
+      this.currentPage = val
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  .wu-pagination
+   margin-top 24px
+   text-align center
+</style>
