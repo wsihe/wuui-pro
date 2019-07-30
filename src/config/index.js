64e786fce5a66d500edd62e6c325aa76024f14a5
river <837606100@qq.com>
2019-07-30 08:53:20 +0800

    [项目] 非测试环境使用生产配置

--- src/config/index.js
+++ src/config/index.js
@@ -1,15 +1,16 @@
 import debugConfig from './debug.config'
 import testConfig from './test.config'
-import releaseConfig from './/release.config'
+import releaseConfig from './release.config'
 
 const configMaps = {
   'localhost': debugConfig,
   'wuui.top': testConfig,
   'release': releaseConfig
 }
+// 非测试环境使用生产的配置
 const config = (() => {
   const hostname = location.hostname || 'localhost'
-  return configMaps[hostname]
+  return configMaps[hostname] || configMaps.release
 })()
 
 export default config
